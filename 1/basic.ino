dbb3db51aa7226a2ddda07ac6fdfa24c59b6ca69
SJang1 <git@sjang.dev>
2025-05-02 15:26:30 +0900

    Update basic.ino

--- 1/basic.ino
+++ 1/basic.ino
@@ -12,13 +12,13 @@ void loop() {
     digitalWrite(green, HIGH);
     digitalWrite(yellow, LOW);
     digitalWrite(red, LOW);
-    delay(1000); // Green light for 1 second
+    delay(1000);
     digitalWrite(green, LOW);
     digitalWrite(yellow, HIGH);
     digitalWrite(red, LOW);
-    delay(1000); // Yellow light for 1 seconds
+    delay(1000);
     digitalWrite(green, LOW);
     digitalWrite(yellow, LOW);
     digitalWrite(red, HIGH);
-    delay(1000); // Red light for 1 seconds
+    delay(1000);
 }
